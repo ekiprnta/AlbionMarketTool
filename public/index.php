9c62497df3d44ea55709b488cd008d34fcaae645
m-zierdt <zierdt@mehrkanal.com>
2022-10-14 14:44:52 +0200

    added bootstrap.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,20 @@ declare(strict_types=1);
 
 use MZierdt\Albion\HttpClient;
 use MZierdt\Albion\Service\ApiService;
+use Twig\Environment;
+
+
+require __DIR__ . '/../src/bootstrap.php';
+require __DIR__ . '/../src/routes.php';
+
+/** @var Environment $twigEnvironment */
+
+
+
+
+
+
+
 
 $uri = $_SERVER['REQUEST_URI'];
 echo 'bla';
