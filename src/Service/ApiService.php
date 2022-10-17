2c86b4fda0f6e2d630ea30f7b2ce3bb7d532e6fa
m-zierdt <zierdt@mehrkanal.com>
2022-10-17 14:22:25 +0200

    refactored the constants

--- src/Service/ApiService.php
+++ src/Service/ApiService.php
@@ -9,11 +9,13 @@ use MZierdt\Albion\HttpClient;
 class ApiService // Buy Order ist buy_price_max
 {
     private const BASE_URL = 'https://www.albion-online-data.com/api/v2/stats/prices/';
-    private const BASE_PLANKS = 'T2_Planks,T3_Planks,T4_Planks,T5_Planks,T6_Planks,T7_Planks,T8_Planks,T4_Planks_level1@1,T5_Planks_level1@1,T6_Planks_level1@1,T7_Planks_level1@1,T8_Planks_level1@1,T4_Planks_level2@2,T5_Planks_level2@2,T6_Planks_level2@2,T7_Planks_level2@2,T8_Planks_level2@2,T4_Planks_level3@3,T5_Planks_level3@3,T6_Planks_level3@3,T7_Planks_level3@3,T8_Planks_level3@3';
-    private const BASE_STONEBLOCK = 'T2_StoneBlock,T3_StoneBlock,T4_StoneBlock,T5_StoneBlock,T6_StoneBlock,T7_StoneBlock,T8_StoneBlock';
-    private const BASE_METALBAR = 'T2_MetalBar,T3_MetalBar,T4_MetalBar,T5_MetalBar,T6_MetalBar,T7_MetalBar,T8_MetalBar,T4_MetalBar_level1@1,T5_MetalBar_level1@1,T6_MetalBar_level1@1,T7_MetalBar_level1@1,T8_MetalBar_level1@1,T4_MetalBar_level2@2,T5_MetalBar_level2@2,T6_MetalBar_level2@2,T7_MetalBar_level2@2,T8_MetalBar_level2@2,T4_MetalBar_level3@3,T5_MetalBar_level3@3,T6_MetalBar_level3@3,T7_MetalBar_level3@3,T8_MetalBar_level3@3';
-    private const BASE_LEATHER = 'T2_Leather,T3_Leather,T4_Leather,T5_Leather,T6_Leather,T7_Leather,T8_Leather,T4_Leather_level1@1,T5_Leather_level1@1,T6_Leather_level1@1,T7_Leather_level1@1,T8_Leather_level1@1,T4_Leather_level2@2,T5_Leather_level2@2,T6_Leather_level2@2,T7_Leather_level2@2,T8_Leather_level2@2,T4_Leather_level3@3,T5_Leather_level3@3,T6_Leather_level3@3,T7_Leather_level3@3,T8_Leather_level3@3';
-    private const BASE_CLOTH = 'T2_Cloth,T3_Cloth,T4_Cloth,T5_Cloth,T6_Cloth,T7_Cloth,T8_Cloth,T4_Cloth_level1@1,T5_Cloth_level1@1,T6_Cloth_level1@1,T7_Cloth_level1@1,T8_Cloth_level1@1,T4_Cloth_level2@2,T5_Cloth_level2@2,T6_Cloth_level2@2,T7_Cloth_level2@2,T8_Cloth_level2@2,T4_Cloth_level3@3,T5_Cloth_level3@3,T6_Cloth_level3@3,T7_Cloth_level3@3,T8_Cloth_level3@3';
+    private const RESOURCE_TIERS_WITH_PLACEHOLDER = 'T2_%s,T3_%s,T4_%s,T5_%s,T6_%s,T7_%s,T8_%s,T4_%s_level1@1,T5_%s_level1@1,T6_%s_level1@1,T7_%s_level1@1,T8_%s_level1@1,T4_%s_level2@2,T5_%s_level2@2,T6_%s_level2@2,T7_%s_level2@2,T8_%s_level2@2,T4_%s_level3@3,T5_%s_level3@3,T6_%s_level3@3,T7_%s_level3@3,T8_%s_level3@3';
+
+    private const RESOURCE_PLANKS = 'Planks';
+    private const RESOURCE_STONEBLOCK = 'T2_StoneBlock,T3_StoneBlock,T4_StoneBlock,T5_StoneBlock,T6_StoneBlock,T7_StoneBlock,T8_StoneBlock';
+    private const RESOURCE_METALBAR = 'MetalBar';
+    private const RESOURCE_LEATHER = 'Leather';
+    private const RESOURCE_CLOTH = 'Cloth';
 
     private const CITY_LYMHURST = 'Lymhurst';
     private const CITY_FORTSTERLING = 'FortSterling';
@@ -63,16 +65,13 @@ class ApiService // Buy Order ist buy_price_max
     {
     }
 
-    /**
-     * @throws \JsonException
-     */
-    public function getResource(string $resourceType): array
+    public function getResource(string $resourceType)
     {
-        $baseURL = match ($resourceType) {
-            'metalBar' => self::BASE_URL . self::BASE_METALBAR,
-            'planks' => self::BASE_URL . self::BASE_PLANKS,
-            'cloth' => self::BASE_URL . self::BASE_CLOTH,
-            'leather' => self::BASE_URL . self::BASE_LEATHER,
+        $apiUrl = match ($resourceType) {
+            'metalBar' => self::BASE_URL . $this->apiUrlAssembler(self::RESOURCE_METALBAR),
+            'planks' => self::BASE_URL . $this->apiUrlAssembler(self::RESOURCE_PLANKS),
+            'cloth' => self::BASE_URL . $this->apiUrlAssembler(self::RESOURCE_CLOTH),
+            'leather' => self::BASE_URL . $this->apiUrlAssembler(self::RESOURCE_LEATHER),
         };
 
         $cities = sprintf(
@@ -83,16 +82,51 @@ class ApiService // Buy Order ist buy_price_max
             self::CITY_MARTLOCK,
             self::CITY_THETFORD
         );
-
-        return $this->jsonDecode($this->httpClient->get($baseURL, ['locations' => $cities]));
+        return $this->jsonDecode($this->httpClient->get($apiUrl, ['locations' => $cities]));
     }
 
     public function getBlackMarketItem(string $itemName)
     {
+        $apiURL = match ($itemName) {
+            self::ITEM_WARRIOR_HELMET => self::BASE_URL . self::ITEM_WARRIOR_HELMET,
+            self::ITEM_WARRIOR_ARMOR => self::BASE_URL . self::ITEM_WARRIOR_ARMOR,
+            self::ITEM_WARRIOR_BOOTS => self::BASE_URL . self::ITEM_WARRIOR_BOOTS,
+            self::ITEM_WARRIOR_SWORD => self::BASE_URL . self::ITEM_WARRIOR_SWORD,
+            self::ITEM_WARRIOR_AXE => self::BASE_URL . self::ITEM_WARRIOR_AXE,
+            self::ITEM_WARRIOR_MACE => self::BASE_URL . self::ITEM_WARRIOR_MACE,
+            self::ITEM_WARRIOR_HAMMER => self::BASE_URL . self::ITEM_WARRIOR_HAMMER,
+            self::ITEM_WARRIOR_WAR_GLOVE => self::BASE_URL . self::ITEM_WARRIOR_WAR_GLOVE,
+            self::ITEM_WARRIOR_CROSSBOW => self::BASE_URL . self::ITEM_WARRIOR_CROSSBOW,
+            self::ITEM_WARRIOR_SHIELD => self::BASE_URL . self::ITEM_WARRIOR_SHIELD,
+            self::ITEM_MAGE_HELMET => self::BASE_URL . self::ITEM_MAGE_HELMET,
+            self::ITEM_MAGE_ARMOR => self::BASE_URL . self::ITEM_MAGE_ARMOR,
+            self::ITEM_MAGE_BOOTS => self::BASE_URL . self::ITEM_MAGE_BOOTS,
+            self::ITEM_MAGE_FIRE_STAFF => self::BASE_URL . self::ITEM_MAGE_FIRE_STAFF,
+            self::ITEM_MAGE_HOLY_STAFF => self::BASE_URL . self::ITEM_MAGE_HOLY_STAFF,
+            self::ITEM_MAGE_ARCANE_STAFF => self::BASE_URL . self::ITEM_MAGE_ARCANE_STAFF,
+            self::ITEM_MAGE_FROST_STAFF => self::BASE_URL . self::ITEM_MAGE_FROST_STAFF,
+            self::ITEM_MAGE_CURSE_STAFF => self::BASE_URL . self::ITEM_MAGE_CURSE_STAFF,
+            self::ITEM_MAGE_TOME_STAFF => self::BASE_URL . self::ITEM_MAGE_TOME_STAFF,
+            self::ITEM_HUNTER_HELMET => self::BASE_URL . self::ITEM_HUNTER_HELMET,
+            self::ITEM_HUNTER_ARMOR => self::BASE_URL . self::ITEM_HUNTER_ARMOR,
+            self::ITEM_HUNTER_BOOTS => self::BASE_URL . self::ITEM_HUNTER_BOOTS,
+            self::ITEM_HUNTER_BOW => self::BASE_URL . self::ITEM_HUNTER_BOW,
+            self::ITEM_HUNTER_SPEAR => self::BASE_URL . self::ITEM_HUNTER_SPEAR,
+            self::ITEM_HUNTER_NATURE_STAFF => self::BASE_URL . self::ITEM_HUNTER_NATURE_STAFF,
+            self::ITEM_HUNTER_DAGGER => self::BASE_URL . self::ITEM_HUNTER_DAGGER,
+            self::ITEM_HUNTER_QUARTERSTAFF => self::BASE_URL . self::ITEM_HUNTER_QUARTERSTAFF,
+            self::ITEM_HUNTER_TORCH => self::BASE_URL . self::ITEM_HUNTER_TORCH,
+        };
 
+        return [];
     }
 
-    private function jsonDecode(string $json): array
+    private function apiUrlAssembler(string $replacement, string $stringWithPlaceholders = self::RESOURCE_TIERS_WITH_PLACEHOLDER)
+    {
+        return str_replace('%s', $replacement, $stringWithPlaceholders);
+    }
+
+    private function jsonDecode(string $json)
     {
         return json_decode($json, true, 512, JSON_THROW_ON_ERROR);
     }
