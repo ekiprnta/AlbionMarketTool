29d16a9f0f8a7162134551031d9712eabe082212
m-zierdt <zierdt@mehrkanal.com>
2022-10-18 17:08:48 +0200

    ResourceRepository gets all Resources

--- src/Handler/ShowResourcePriceHandler.php
+++ src/Handler/ShowResourcePriceHandler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MZierdt\Albion\Handler;
 
 use Laminas\Diactoros\Response\HtmlResponse;
+use MZierdt\Albion\repositories\ResourceRepository;
 use MZierdt\Albion\repositories\ResourceUploadRepository;
 use MZierdt\Albion\repositories\UploadCsvRepository;
 use Twig\Environment;
@@ -15,13 +16,17 @@ class ShowResourcePriceHandler
         private Environment $twigEnvironment,
         private UploadCsvRepository $uploadCsvRepository,
         private ResourceUploadRepository $repositoryUpload,
+
     ) {
     }
 
     public function handler(): HtmlResponse
     {
-        $this->uploadCsvRepository->fillItemsCsvFiles();
+//        $this->uploadCsvRepository->fillItemsCsvFiles();
 //        $this->repositoryUpload->uploadIntoCsv();
+
+        $bla = new ResourceRepository();
+        $bla->getResourcesAsResourceEntity();
 
         $htmlContent = $this->twigEnvironment->render('test.html.twig');
         return new HtmlResponse($htmlContent);
---
+++ src/repositories/ResourceRepository.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MZierdt\Albion\repositories;
+
+use League\Csv\Reader;
+use MZierdt\Albion\Entity\ResourceEntity;
+
+class ResourceRepository
+{
+    private const PATH_TO_CSV = __DIR__ . '/../../assets/ressourcen.csv';
+
+    public function __construct()
+    {
+    }
+
+    public function getResourcesAsResourceEntity(): ResourceEntity
+    {
+        $reader = $this->getReader();
+
+        $allResourceInformation = $reader->getRecords();
+        $this->dataAsResourceEntity($allResourceInformation);
+
+
+        return new ResourceEntity();
+    }
+
+    private function getReader(): Reader
+    {
+        $csv = Reader::createFromPath(self::PATH_TO_CSV, 'rb');
+        $csv->setHeaderOffset(0);
+        return $csv;
+    }
+
+    private function dataAsResourceEntity(iterable $allResourceInformation)
+    {
+        $metalBarStack = [];
+        $planksStack = [];
+        $clothStack = [];
+        $leatherStack = [];
+        foreach ($allResourceInformation as $resource) {
+            $lowerCaseItemId = strtolower($resource['itemId']);
+            if (str_contains($lowerCaseItemId, 'metalbar')) {
+                $metalBarStack[] = new ResourceEntity($resource);
+            }
+            if (str_contains($lowerCaseItemId, 'planks')) {
+                $planksStack[] = new ResourceEntity($resource);
+            }
+            if (str_contains($lowerCaseItemId, 'cloth')) {
+                $clothStack[] = new ResourceEntity($resource);
+            }
+            if (str_contains($lowerCaseItemId, 'leather')) {
+                $leatherStack[] = new ResourceEntity($resource);
+            }
+        }
+        return [
+            'metalBar' => $metalBarStack,
+            'planks' => $planksStack,
+            'cloth' => $clothStack,
+            'leather' => $leatherStack
+        ];
+    }
+}
