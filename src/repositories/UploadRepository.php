9549599b011a35a4249f6ef872b21b993c89a1fb
m-zierdt <zierdt@mehrkanal.com>
2022-10-25 23:08:48 +0200

    ## Bugfix Item Prices will now be updated ## Bugfix No Focus price now correctly calculated

--- src/repositories/UploadRepository.php
+++ src/repositories/UploadRepository.php
@@ -46,7 +46,7 @@ SQL;
         }
     }
 
-    public function reloadUpdatedPrices(array $resourceInformation): void
+    public function reloadUpdatedPricesResources(array $resourceInformation): void
     {
         foreach ($resourceInformation as $information) {
             if ($information['sellOrderPrice'] !== '0') {
@@ -76,6 +76,48 @@ SQL
                     WHERE albion_db.resource.name = :name
                     AND albion_db.resource.tier = :tier
                     AND albion_db.resource.city = :city
+SQL
+                );
+                $statement->bindParam(':buyOrderPrice', $information['buyOrderPrice']);
+                $statement->bindParam(':buyOrderPriceDate', $information['buyOrderPriceDate']);
+                $statement->bindParam(':name', $information['name']);
+                $statement->bindParam(':tier', $information['tier']);
+                $statement->bindParam(':city', $information['city']);
+                $statement->execute();
+            }
+        }
+    }
+
+    public function reloadUpdatedPricesItems(array $itemInformation): void
+    {
+        foreach ($itemInformation as $information) {
+            if ($information['sellOrderPrice'] !== '0') {
+                $statement = $this->pdoConnection->prepare(
+                    <<<SQL
+                    UPDATE albion_db.items
+                    SET `sellOrderPrice` = :sellOrderPrice,
+                        `sellOrderPriceDate` = :sellOrderPriceDate
+                    WHERE albion_db.items.name = :name
+                    AND albion_db.items.tier = :tier
+                    AND albion_db.items.city = :city
+SQL
+                );
+                $statement->bindParam(':sellOrderPrice', $information['sellOrderPrice']);
+                $statement->bindParam(':sellOrderPriceDate', $information['sellOrderPriceDate']);
+                $statement->bindParam(':name', $information['name']);
+                $statement->bindParam(':tier', $information['tier']);
+                $statement->bindParam(':city', $information['city']);
+                $statement->execute();
+            }
+            if ($information['buyOrderPrice'] !== '0') {
+                $statement = $this->pdoConnection->prepare(
+                    <<<SQL
+                    UPDATE albion_db.items
+                    SET `buyOrderPrice` = :buyOrderPrice,
+                        `buyOrderPriceDate` = :buyOrderPriceDate
+                    WHERE albion_db.items.name = :name
+                    AND albion_db.items.tier = :tier
+                    AND albion_db.items.city = :city
 SQL
                 );
                 $statement->bindParam(':buyOrderPrice', $information['buyOrderPrice']);
