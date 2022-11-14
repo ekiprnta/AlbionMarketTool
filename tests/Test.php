09d72cd3d96181cf5f6a27717c12ca215f4cb339
m-zierdt <zierdt@mehrkanal.com>
2022-11-14 16:58:32 +0100

    added a test

---
+++ tests/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testTest()
+    {
+        $a = 0;
+        $this->assertEquals(0,$a);
+ }
+}
