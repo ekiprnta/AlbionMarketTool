b418b6c36853abcb1ce842be2146a974af9f9fab
m-zierdt <zierdt@mehrkanal.com>
2022-12-07 09:34:19 +0100

    add Interface

---
+++ src/Service/MarketInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MZierdt\Albion\Service;
+
+interface MarketInterface
+{
+    public const NUTRITION_FACTOR = 0.1125;
+    public const MARKET_SETUP = 0.025;
+    public const MARKET_FEE = 0.04;
+
+    public const PREMIUM_FACTOR = 1.5;
+
+    public const RRR_BASE_PERCENTAGE = 100;
+}
