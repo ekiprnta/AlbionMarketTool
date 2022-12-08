26ffa3cf2b890d2a271a4c224b7cbb6d32bf6d56
m-zierdt <zierdt@mehrkanal.com>
2022-12-08 08:59:11 +0100

    cleanup Service

--- src/Service/BlackMarketTransportingService.php
+++ src/Service/BlackMarketTransportingService.php
@@ -12,8 +12,6 @@ use MZierdt\Albion\repositories\ItemRepository;
 
 class BlackMarketTransportingService
 {
-    private int $maxWeight;
-
     public function __construct(private ItemRepository $itemRepository)
     {
     }
@@ -70,72 +68,18 @@ class BlackMarketTransportingService
             $bmtEntity->setThProfitGrade(BlackMarketTransportingHelper::calculateProfitGrade($bmtEntity->getFsWeightProfitQuotient()));
 
         }
-
-
 //        $combinedItems = $this->combineItems($cityItems, $bmItems);
         return $this->filterItems($bmtEntities, $tierList);
     }
 
-
-    private function calculateColorGrade(?float $quotient): string
-    {
-        if ($quotient === null) {
-            return 'D';
-        }
-        return match (true) {
-            $quotient >= 10000 => 'S',
-            $quotient >= 5000 => 'A',
-            $quotient >= 1000 => 'B',
-            $quotient >= 500 => 'C',
-            default => 'D',
-        };
-    }
-
-    private function addCityItems(BlackMarketTransportEntity $transportEntity, array $cityItems): void
-    {
-        /** @var ItemEntity $cityItem */
-        foreach ($cityItems as $cityItem) {
-            if ($cityItem->getTier() === $transportEntity->getTier() &&
-                $cityItem->getName() === $transportEntity->getName()) {
-                $transportEntity->setCityPrice($cityItem->getSellOrderPrice());
-                $transportEntity->setCityPriceDate($cityItem->getSellOrderPriceDate());
-                $transportEntity->setAmount((int) ceil($this->maxWeight / $transportEntity->getWeight()));
-                $transportEntity->setCityProfit(
-                    (int) ($transportEntity->getBmPrice() *
-                        (1 - BlackMarketCraftingService::MARKET_SETUP - BlackMarketCraftingService::MARKET_FEE) -
-                        $transportEntity->getCityPrice())
-                );
-                $transportEntity->setCityWeightProfitQuotient(
-                    $transportEntity->getCityProfit() / $transportEntity->getWeight()
-                );
-                $transportEntity->setCityColorGrade(
-                    $this->calculateColorGrade($transportEntity->getCityWeightProfitQuotient())
-                );
-                $transportEntity->setCityPriceAge($this->getTimeDiff($transportEntity->getCityPriceDate()));
-                $transportEntity->setBmPriceAge($this->getTimeDiff($transportEntity->getBmPriceDate()));
-                $transportEntity->setTotalProfit($transportEntity->getAmount() * $transportEntity->getCityProfit());
-            }
-        }
-    }
-
-    private function getTimeDiff(?DateTimeImmutable $priceDate): int
-    {
-        if ($priceDate === null) {
-            return 999999;
-        }
-        $now = \DateTimeImmutable::createFromFormat('Y-m-d H:i:s', Date('Y-m-d H:i:s'));
-        $itemDiff = date_diff($now, $priceDate);
-        return $itemDiff->d * 24 * 60 + $itemDiff->h * 60 + $itemDiff->i;
-    }
-
-    private function filterItems(array $combinedItems, array $tierList): array
+    private function filterItems(array $bmtEntities, array $tierList): array
     {
         /** @var BlackMarketTransportEntity $combinedItem */
-        foreach ($combinedItems as $key => $combinedItem) {
-            if (! in_array($combinedItem->getTier(), $tierList, true)) {
-                unset($combinedItems[$key]);
+        foreach ($bmtEntities as $key => $bmtEntity) {
+            if (! in_array($combinedItem->getBmItem()->getTier(), $tierList, true)) {
+                unset($bmtEntity[$key]);
             }
         }
-        return $combinedItems;
+        return $bmtEntities;
     }
 }
