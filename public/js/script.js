81c0e09d4f028f9fa78fcb591f3ee5e4c58c0f55
m-zierdt <zierdt@mehrkanal.com>
2023-02-02 14:28:30 +0100

    cleanup Jquery and Bootstrap

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#sortTable').DataTable({'pageLength': 50});
+
+  $('#toggleSellBuy').on('click', function() {
+    $('#resourceSell').toggle();
+    $('#resourceBuy').toggle();
+  });
+
+  $('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').trigger('focus');
+  });
+
+});
