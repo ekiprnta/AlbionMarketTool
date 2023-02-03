c37e469e6f17efeaeaa190be78966408232d9b74
m-zierdt <zierdt@mehrkanal.com>
2023-02-03 19:17:04 +0100

    023 | Add ListDataHelperTest.php

---
+++ tests/unit/Service/ListDataHelperTest.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace unit\Service;
+
+use MZierdt\Albion\Entity\ListDataEntity;
+use MZierdt\Albion\Entity\ResourceEntity;
+use MZierdt\Albion\Service\ListDataHelper;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+class ListDataHelperTest extends TestCase
+{
+    use ProphecyTrait;
+
+    private ListDataHelper $listDataHelper;
+
+    /** @dataProvider provideItemObjects */
+    public function testCalculateSameItemObject(
+        ?ResourceEntity $result,
+        ResourceEntity $startResource,
+        array $resources
+    ): void {
+        $ldEntity = new ListDataEntity($startResource);
+
+        $this->assertEquals($result, $this->listDataHelper->calculateSameItemObject($ldEntity, $resources));
+    }
+
+    public function provideItemObjects(): array
+    {
+        $resourceA = new ResourceEntity([
+            'tier' => '2',
+            'name' => 'cloth',
+            'city' => 'Fort Sterling',
+            'realName' => 'cloth',
+            'sellOrderPrice' => '27',
+            'sellOrderPriceDate' => '2022-12-08 10:45:00',
+            'buyOrderPrice' => '26',
+            'buyOrderPriceDate' => '2022-12-08 10:45:00',
+            'bonusCity' => 'Lymhurst',
+            'amountInStorage' => null,
+        ]);
+        return [
+            [$resourceA, $resourceA, [$resourceA]],
+            [null, $resourceA, []],
+        ];
+    }
+
+    /** @dataProvider provideCheapPrices */
+    public function testCalculateCheapestCity(
+        string $result,
+        int $fsPrice,
+        int $lymPrice,
+        int $bwPrice,
+        int $mlPrice,
+        int $thPrice,
+    ): void {
+        $this->assertEquals(
+            $result,
+            $this->listDataHelper->calculateCheapestCity($fsPrice, $lymPrice, $bwPrice, $mlPrice, $thPrice)
+        );
+    }
+
+    public function provideCheapPrices(): array
+    {
+        return [
+            ['Fort Sterling', 1, 2, 3, 4, 5],
+            ['Lymhurst', 1, 0, 3, 4, 5],
+            ['Bridgewatch', 1, 2, 0, 4, 5],
+            ['Martlock', 1, 2, 3, 0, 5],
+            ['Thetford', 1, 2, 3, 4, 0],
+        ];
+    }
+
+    /** @dataProvider providePrices */
+    public function testCalculateMOstExpensiveCity(
+        string $result,
+        int $fsPrice,
+        int $lymPrice,
+        int $bwPrice,
+        int $mlPrice,
+        int $thPrice,
+    ): void {
+        $this->assertEquals(
+            $result,
+            $this->listDataHelper->calculateMostExpensiveCity($fsPrice, $lymPrice, $bwPrice, $mlPrice, $thPrice)
+        );
+    }
+
+    public function providePrices(): array
+    {
+        return [
+            ['Fort Sterling', 10, 2, 3, 4, 5],
+            ['Lymhurst', 1, 10, 3, 4, 5],
+            ['Bridgewatch', 1, 2, 10, 4, 5],
+            ['Martlock', 1, 2, 3, 10, 5],
+            ['Thetford', 1, 2, 3, 4, 10],
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        $this->listDataHelper = new ListDataHelper();
+    }
+}
