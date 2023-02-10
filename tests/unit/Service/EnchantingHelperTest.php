dc380502348fa53988bd85f289c80551e28c7ec8
m-zierdt <zierdt@mehrkanal.com>
2023-02-10 09:42:14 +0100

    033 | WIP Tests

---
+++ tests/unit/Service/EnchantingHelperTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace unit\Service;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+class EnchantingHelperTest extends TestCase
+{
+    use ProphecyTrait;
+}
