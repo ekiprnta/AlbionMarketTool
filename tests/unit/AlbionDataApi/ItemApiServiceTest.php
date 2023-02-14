65b6b0b260e923f56bbb5d8dd60b5b0495cbe723
m-zierdt <zierdt@mehrkanal.com>
2023-02-14 19:47:11 +0100

    028 | apply changes to Albion Data

--- tests/unit/AlbionDataApi/ItemApiServiceTest.php
+++ tests/unit/AlbionDataApi/ItemApiServiceTest.php
@@ -31,6 +31,19 @@ class ItemApiServiceTest extends TestCase
         $this->assertEquals(['a' => 'b'], $this->itemApiService->getItems('HEAD_PLATE_SET1'));
     }
 
+    public function testGetCapes(): void
+    {
+        $apiUrl = 'https://www.albion-online-data.com/api/v2/stats/prices/T2_HEAD_PLATE_SET1,T3_HEAD_PLATE_SET1,T4_HEAD_PLATE_SET1,T5_HEAD_PLATE_SET1,T6_HEAD_PLATE_SET1,T7_HEAD_PLATE_SET1,T8_HEAD_PLATE_SET1,T4_HEAD_PLATE_SET1@1,T5_HEAD_PLATE_SET1@1,T6_HEAD_PLATE_SET1@1,T7_HEAD_PLATE_SET1@1,T8_HEAD_PLATE_SET1@1,T4_HEAD_PLATE_SET1@2,T5_HEAD_PLATE_SET1@2,T6_HEAD_PLATE_SET1@2,T7_HEAD_PLATE_SET1@2,T8_HEAD_PLATE_SET1@2,T4_HEAD_PLATE_SET1@3,T5_HEAD_PLATE_SET1@3,T6_HEAD_PLATE_SET1@3,T7_HEAD_PLATE_SET1@3,T8_HEAD_PLATE_SET1@3,T4_HEAD_PLATE_SET1@4,T5_HEAD_PLATE_SET1@4,T6_HEAD_PLATE_SET1@4,T7_HEAD_PLATE_SET1@4,T8_HEAD_PLATE_SET1@4';
+        $parameters = [
+            'locations' => ApiService::CITY_ALL,
+            'qualities' => ApiService::QUALITY_GOOD,
+        ];
+        $this->httpClient->get($apiUrl, $parameters)
+            ->willReturn('{"a": "b"}');
+
+        $this->assertEquals(['a' => 'b'], $this->itemApiService->getCapes('HEAD_PLATE_SET1'));
+    }
+
     protected function setUp(): void
     {
         $this->httpClient = $this->prophesize(HttpClient::class);
