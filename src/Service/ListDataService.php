980d3b8f80c1c4ea8dff6a83e459b13aed2a2d77
m-zierdt <zierdt@mehrkanal.com>
2023-02-17 09:53:39 +0100

    042 | add raw Resources to ListDataHandler.php

--- src/Service/ListDataService.php
+++ src/Service/ListDataService.php
@@ -13,30 +13,25 @@ class ListDataService
     ) {
     }
 
-    public function getAllRawResources(): array
+    public function getResources(string $type): array
     {
-        $fortSterlingItems = $this->resourceRepository->getRawResourcesByCity('Fort Sterling');
-        $lymhurstItems = $this->resourceRepository->getRawResourcesByCity('Lymhurst');
-        $bridgewatchItems = $this->resourceRepository->getRawResourcesByCity('Bridgewatch');
-        $martlockItems = $this->resourceRepository->getRawResourcesByCity('Martlock');
-        $thetfordItems = $this->resourceRepository->getRawResourcesByCity('Thetford');
-
-        return $this->getListDataEntities(
-            $fortSterlingItems,
-            $lymhurstItems,
-            $bridgewatchItems,
-            $martlockItems,
-            $thetfordItems
-        );
-    }
-
-    public function getAllResources(): array
-    {
-        $fortSterlingItems = $this->resourceRepository->getResourcesByCity('Fort Sterling');
-        $lymhurstItems = $this->resourceRepository->getResourcesByCity('Lymhurst');
-        $bridgewatchItems = $this->resourceRepository->getResourcesByCity('Bridgewatch');
-        $martlockItems = $this->resourceRepository->getResourcesByCity('Martlock');
-        $thetfordItems = $this->resourceRepository->getResourcesByCity('Thetford');
+        if ($type === 'rawResource') {
+            $fortSterlingItems = $this->resourceRepository->getRawResourcesByCity('Fort Sterling');
+            $lymhurstItems = $this->resourceRepository->getRawResourcesByCity('Lymhurst');
+            $bridgewatchItems = $this->resourceRepository->getRawResourcesByCity('Bridgewatch');
+            $martlockItems = $this->resourceRepository->getRawResourcesByCity('Martlock');
+            $thetfordItems = $this->resourceRepository->getRawResourcesByCity('Thetford');
+        } else {
+            if ($type === 'resource') {
+                $fortSterlingItems = $this->resourceRepository->getResourcesByCity('Fort Sterling');
+                $lymhurstItems = $this->resourceRepository->getResourcesByCity('Lymhurst');
+                $bridgewatchItems = $this->resourceRepository->getResourcesByCity('Bridgewatch');
+                $martlockItems = $this->resourceRepository->getResourcesByCity('Martlock');
+                $thetfordItems = $this->resourceRepository->getResourcesByCity('Thetford');
+            } else {
+                throw new \InvalidArgumentException('Wrong Type int getResources ' . $type);
+            }
+        }
 
         return $this->getListDataEntities(
             $fortSterlingItems,
