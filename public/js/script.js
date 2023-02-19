38cb2e41081894e51c26fad74052b5b41a176550
m-zierdt <zierdt@mehrkanal.com>
2023-02-19 20:27:23 +0100

    043 | you can now select Tiers for Transport

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,28 @@
 $(function() {
-  $('.sortTable').DataTable({'pageLength': 50});
+  let sellOrder = true;
+
+  $('.sortTable').DataTable({'pageLength': 1000});
 
   $('#toggleSellBuy').on('click', function() {
     $('#resourceSell').toggle();
     $('#resourceBuy').toggle();
+    sellOrder = !sellOrder;
   });
 
   $('#myModal').on('shown.bs.modal', function() {
     $('#myInput').trigger('focus');
+  });
+
+  $('.tierSelect').on('change', function(e) {
+    let tier = $(this).val();
+    if (sellOrder) {
+      $('.sell' + tier).toggle();
+      console.log('1');
+    } else {
+      $('.buy' + tier).toggle();
+      console.log('2');
+    }
+    console.log('.sell' + tier);
   });
 
 });
