bba01a8dacaf5e02ce83a0ae61137681305a280d
m-zierdt <zierdt@mehrkanal.com>
2023-02-22 08:34:02 +0100

    043 | add command

--- bin/cli.php
+++ bin/cli.php
@@ -7,6 +7,7 @@ use MZierdt\Albion\commands\UpdateBmTransportCommand;
 use MZierdt\Albion\commands\UpdateItemsCommand;
 use MZierdt\Albion\commands\UpdateJournalsCommand;
 use MZierdt\Albion\commands\UpdateMaterialsCommand;
+use MZierdt\Albion\commands\UpdateRefiningCommand;
 use MZierdt\Albion\commands\UpdateResourcesCommand;
 use Symfony\Component\Console\Application;
 
@@ -20,4 +21,5 @@ $app->add($container->get(UpdateJournalsCommand::class));
 $app->add($container->get(UpdateResourcesCommand::class));
 $app->add($container->get(UpdateMaterialsCommand::class));
 $app->add($container->get(UpdateBmTransportCommand::class));
+$app->add($container->get(UpdateRefiningCommand::class));
 $app->run();
