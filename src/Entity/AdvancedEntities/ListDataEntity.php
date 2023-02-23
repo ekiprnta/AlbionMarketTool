b83c54d9d1e16b748222d9de30bf0eb1e9e85bf8
m-zierdt <zierdt@mehrkanal.com>
2023-02-23 08:36:57 +0100

    043 | Doctrine for ListDataEntity.php

--- src/Entity/AdvancedEntities/ListDataEntity.php
+++ src/Entity/AdvancedEntities/ListDataEntity.php
@@ -2,27 +2,62 @@
 
 namespace MZierdt\Albion\Entity\AdvancedEntities;
 
+use Doctrine\ORM\Mapping\ChangeTrackingPolicy;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Table;
 use MZierdt\Albion\Entity\ItemEntity;
 use MZierdt\Albion\Entity\ResourceEntity;
 
+#[Entity]
+#[ChangeTrackingPolicy('DEFERRED_EXPLICIT')]
+#[Table(name: 'listData')]
 class ListDataEntity
 {
+    #[Column(type: 'integer')]
+    #[Id, GeneratedValue(strategy: 'AUTO')]
+    protected ?int $id = null;
+
+    #[ManyToOne(targetEntity: ResourceEntity::class, cascade: ['persist'])]
+    #[JoinColumn(name: 'fortsterlingObject', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    private ResourceEntity $fortsterlingObject;
+
+    #[ManyToOne(targetEntity: ResourceEntity::class, cascade: ['persist'])]
+    #[JoinColumn(name: 'martlockObject', referencedColumnName: 'id', onDelete: 'CASCADE')]
     private ResourceEntity $martlockObject;
+
+    #[ManyToOne(targetEntity: ResourceEntity::class, cascade: ['persist'])]
+    #[JoinColumn(name: 'bridgewatchObject', referencedColumnName: 'id', onDelete: 'CASCADE')]
     private ResourceEntity $bridgewatchObject;
+
+    #[ManyToOne(targetEntity: ResourceEntity::class, cascade: ['persist'])]
+    #[JoinColumn(name: 'lymhurstObject', referencedColumnName: 'id', onDelete: 'CASCADE')]
     private ResourceEntity $lymhurstObject;
+
+    #[ManyToOne(targetEntity: ResourceEntity::class, cascade: ['persist'])]
+    #[JoinColumn(name: 'thetfordObject', referencedColumnName: 'id', onDelete: 'CASCADE')]
     private ResourceEntity $thetfordObject;
 
-    private string $cheapestObjectCitySellOrder;
-    private string $mostExpensiveObjectCitySellOrder;
+    #[Column(type: 'string', nullable: true)]
+    private ?string $cheapestObjectCitySellOrder;
+    #[Column(type: 'string', nullable: true)]
+    private ?string $mostExpensiveObjectCitySellOrder;
 
-    private string $cheapestObjectCityBuyOrder;
-    private string $mostExpensiveObjectCityBuyOrder;
+    #[Column(type: 'string', nullable: true)]
+    private ?string $cheapestObjectCityBuyOrder;
+    #[Column(type: 'string', nullable: true)]
+    private ?string $mostExpensiveObjectCityBuyOrder;
 
     private readonly int $tierColor;
 
-    public function __construct(private readonly ResourceEntity $fortsterlingObject)
+    public function __construct(ResourceEntity $fortsterlingObject)
     {
-        $this->tierColor = (int) ($this->fortsterlingObject->getTier() / 10);
+        $this->fortsterlingObject = $fortsterlingObject;
+        $this->tierColor = (int) ($fortsterlingObject->getTier() / 10);
     }
 
     public function getTierColor(): int
